2b39b93acb49b7c9b909e0a9405143d5bbaa7e1e
parasruhela <parasruhela2003@gmail.com>
2026-02-24 12:06:42 +0530

    24feb

---
+++ 24feb/100question/Find GCD using while loop.py
@@ -0,0 +1,12 @@
+a = int(input("enter the value1:"))
+b = int(input("enter the value2:"))
+
+i = 1
+gcd = 1
+
+while i <= a and i <= b:
+    if a % i == 0 and b % i == 0:
+        gcd = i
+    i += 1
+
+print(gcd)
