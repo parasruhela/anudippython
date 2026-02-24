adfa05c89f1eb2136d99f0d70d0383d91c1a22d0
parasruhela <parasruhela2003@gmail.com>
2026-02-24 11:29:28 +0530

    24feb

---
+++ 24feb/100question/Find the greatest of four numbers.py
@@ -0,0 +1,6 @@
+a = float(input("enter the number1:"))
+b = float(input("enter the number2:"))
+c = float(input("enter the number3:"))
+d = float(input("enter the number4:"))
+
+print(max(a, b, c, d))
