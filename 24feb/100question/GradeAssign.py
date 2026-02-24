ac54512c6d84b10af8f2efa9cf0aef393bd00b8c
parasruhela <parasruhela2003@gmail.com>
2026-02-24 11:01:30 +0530

    24feb

---
+++ 24feb/100question/GradeAssign.py
@@ -0,0 +1,14 @@
+marks = int(input())
+
+if marks >= 90 and marks <= 100:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+elif marks >= 60:
+    print("Grade D")
+elif marks >= 0:
+    print("Grade F")
+else:
+    print("Invalid Marks")
